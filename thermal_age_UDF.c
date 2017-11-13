19d512fac1fa422a0a4983a0a71a8b5ea86e5cc8
anamolpundle <anamolpundle@gmail.com>
2017-11-12 16:42:37 -0800

    changed thermal age calculation

--- thermal_age_UDF.c
+++ thermal_age_UDF.c
@@ -11,15 +11,15 @@ DEFINE_INIT(thermal_age_setup,domain)
 
 DEFINE_DPM_SCALAR_UPDATE(thermal_age, cell, thread, initialize, p)
 {
-	cellTemp = C_T(cell, thread);
+	real particleTemp = P_T(p);
 	if(initialize)
 	{
 		P_USER_REAL(p,0) = 0.0;
-		P_USER_REAL(p,1) = cellTemp;
+		P_USER_REAL(p,1) = particleTemp;
 	}
 	else
 	{
-		P_USER_REAL(p,0) = P_USER_REAL(p,0) + P_DT(p) * 0.5 * cellTemp;
-		P_USER_CELL(p,1) = cellTemp;
+		P_USER_REAL(p,0) = P_USER_REAL(p,0) + P_DT(p) * 0.5 * P_USER_CELL(p,1) * particleTemp;
+		P_USER_CELL(p,1) = particleTemp;
 	}
 }
