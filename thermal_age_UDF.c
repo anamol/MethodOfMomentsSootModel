2cfb5e3e9be97fe3a5d6810bcd68c1be7642b751
anamolpundle <anamolpundle@gmail.com>
2017-11-14 09:32:29 -0800

    added INIT function for reading values from csv file

--- thermal_age_UDF.c
+++ thermal_age_UDF.c
@@ -1,4 +1,7 @@
 #include "udf.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
 DEFINE_INIT(thermal_age_setup,domain)
 {
@@ -21,5 +24,56 @@ DEFINE_DPM_SCALAR_UPDATE(thermal_age, cell, thread, initialize, p)
 	{
 		P_USER_REAL(p,0) = P_USER_REAL(p,0) + P_DT(p) * 0.5 * P_USER_CELL(p,1) * particleTemp;
 		P_USER_CELL(p,1) = particleTemp;
+	}
+}
+
+DEFINE_INIT(init_alpha,d)
+{
+	FILE* file = fopen("cell_values_alpha.csv", "r");
+
+	char line[256];
+
+	int cell_id[62400]; 
+	float thermal_age[62400];
+	float thermal_std[62400];
+	int i = 0;
+	const char s[2] = ",";
+
+	while (fgets(line, sizeof(line), file))
+	{
+		
+		char *token;
+
+		token = strtok(line,s);
+		cell_id[i] = atoi(token);
+
+		token = strtok(NULL,s);
+		thermal_age[i] = atof(token);
+
+		token = strtok(NULL,s);
+		thermal_std[i] = atof(token);
+
+		token = strtok(NULL,s);
+		alpha[i] = atof(token);
+
+		i = i + 1;
+	}
+
+	fclose(file);
+
+	cell_t c;
+	Thread* T;
+	real xc[ND_ND];
+
+	thread_loop_c(t,d)
+	{
+		begin_c_loop_all(c,t)
+		{
+			int cell_number = c;
+			C_UDMI(c,t,30) = thermal_age[cell_number - 1];
+			C_UDMI(c,t,31) = thermal_std[cell_number - 1];
+			C_UDMI(c,t,32) = alpha[cell_number - 1];
+
+		}
 	}
 }
