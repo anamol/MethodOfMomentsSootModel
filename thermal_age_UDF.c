dfda42ee7ae2030acbab0427f2efbff9e2275966
Anamol Pundle <anamolpundle@gmail.com>
2017-11-15 13:56:35 -0800

    corrected errors in define_init

--- thermal_age_UDF.c
+++ thermal_age_UDF.c
@@ -3,6 +3,8 @@
 #include "string.h"
 #include "stdlib.h"
 
+#define mesh_size 62400
+
 DEFINE_INIT(thermal_age_setup,domain)
 {
 	if(NULLP(user_particle_vars)) Init_User_Particle_Vars();
@@ -15,6 +17,7 @@ DEFINE_INIT(thermal_age_setup,domain)
 DEFINE_DPM_SCALAR_UPDATE(thermal_age, cell, thread, initialize, p)
 {
 	real particleTemp = P_T(p);
+
 	if(initialize)
 	{
 		P_USER_REAL(p,0) = 0.0;
@@ -23,20 +26,22 @@ DEFINE_DPM_SCALAR_UPDATE(thermal_age, cell, thread, initialize, p)
 	else
 	{
 		P_USER_REAL(p,0) = P_USER_REAL(p,0) + P_DT(p) * 0.5 * P_USER_CELL(p,1) * particleTemp;
-		P_USER_CELL(p,1) = particleTemp;
+		P_USER_REAL(p,1) = particleTemp;
 	}
 }
 
-DEFINE_INIT(init_alpha,d)
+DEFINE_ON_DEMAND(alpha)
 {
-	FILE* file = fopen("cell_values_alpha.csv", "r");
+	FILE* file = fopen("//home/anamol/Fluent/SmallFlameFinerMesh_newPDF/cell_values_alpha.csv", "r");
 
 	char line[256];
 
-	int cell_id[62400]; 
-	float thermal_age[62400];
-	float thermal_std[62400];
-	float alpha[62400];
+	int cell_id[mesh_size]; 
+	float thermal_age[mesh_size];
+	float thermal_std[mesh_size];
+	float alpha[mesh_size];
+	float x_coord[mesh_size];
+	float y_coord[mesh_size];
 	int i = 0;
 	const char s[2] = ",";
 
@@ -57,24 +62,53 @@ DEFINE_INIT(init_alpha,d)
 		token = strtok(NULL,s);
 		alpha[i] = atof(token);
 
+		token = strtok(NULL,s);
+		x_coord[i] = atof(token);
+
+		token = strtok(NULL,s);
+		y_coord[i] = atof(token);
+
+
 		i = i + 1;
 	}
 
 	fclose(file);
 
+	Domain* d;
 	cell_t c;
 	Thread* t;
 	real xc[ND_ND];
 
+	d = Get_Domain(1);
+
 	thread_loop_c(t,d)
 	{
-		begin_c_loop_all(c,t)
+		begin_c_loop(c,t)
 		{
-			int cell_number = c;
-			C_UDMI(c,t,30) = thermal_age[cell_number - 1];
-			C_UDMI(c,t,31) = thermal_std[cell_number - 1];
-			C_UDMI(c,t,32) = alpha[cell_number - 1];
+			C_CENTROID(xc,c,t);
+			float distance[mesh_size];
+			float min_distance = 500.00;
+			int index;
+			int i;
+
+			for(i = 0; i < mesh_size; i++)
+			{
+				distance[i] = pow((xc[0] - x_coord[i]),2) + pow((xc[1] - y_coord[i]),2);
+				
+				if (distance[i] < min_distance) 
+				{
+					min_distance = distance[i];
+					index = i;
+				}
+
+			}
+
+			C_UDMI(c,t,30) = thermal_age[index];
+			C_UDMI(c,t,31) = thermal_std[index];
+			C_UDMI(c,t,32) = alpha[index]; 
+			
 
 		}
+		end_c_loop(c,t)
 	}
 }
