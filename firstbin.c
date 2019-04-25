2ab7bad1ba8ad6bdf6bfe40a1a992aba078c5667
anamolpundle <anamolpundle@gmail.com>
2019-04-25 11:41:30 -0700

    changed growth and ox term in firstbin

--- firstbin.c
+++ firstbin.c
@@ -47,9 +47,9 @@ DEFINE_SOURCE(first_bin_coag,c,t,dS,eqn)
 
     real f_oneHalf = pow(f_0, 3.0/8.0) * pow(f_1, 3.0/4.0) * pow(f_2, -1.0/8.0);
 
-    real Gc = cellN1 * Kc * (2 * cellM0 + pow(m1, oneThird) * MminusOneThird + pow(m1, -oneThird) * MoneThird + \ 
-    	KcPrime * (2 * pow(m1, -oneThird) * cellM0 + MminusOneThird + pow(m1, oneThird) * MminusTwoThird)) * normParameter
-    real Gf = cellN1 * pow(m1, -oneHalf) * Kf * normParameter;
+    real Gc = -cellN1 * Kc * (2 * cellM0 + pow(m1, oneThird) * MminusOneThird + pow(m1, -oneThird) * MoneThird + \ 
+    	KcPrime * (2 * pow(m1, -oneThird) * cellM0 + MminusOneThird + pow(m1, oneThird) * MminusTwoThird)) * normParameter;
+    real Gf = -cellN1 * pow(m1, -oneHalf) * Kf * normParameter;
 
 
     if (Kn <= 0.1)
@@ -82,6 +82,8 @@ DEFINE_SOURCE(first_bin_oxidation,c,t,dS,eqn)
     real cellM2 = cellRho*C_UDSI(c,t,2);
     real cellN1 = cellRho*C_UDSI(c,t,3);
 
+    Material *mat = THREAD_MATERIAL(t);
+
     int idO2 = mixture_specie_index(mat, "o2");
     int idOH = mixture_specie_index(mat, "oh");
 
@@ -90,16 +92,23 @@ DEFINE_SOURCE(first_bin_oxidation,c,t,dS,eqn)
 
     real O2Mc = O2Mf*cellRho/O2MW;
     real OHMc = OHMf*cellRho/OHMW;
+
+    real chiSoot = chiSootCalc(c, t, cellTemp, cellRho);
+
+    real a = a1 + a2*cellTemp;
+    real b = b1 + b2*cellTemp;
+    real alpha = tanh(a/log10(cellM1/cellM0)+b); 
+    if (alpha < 0 ) { alpha = 0.01; }
     
     real k5 = A5*exp(-Ea5/(R*cellTemp)) * O2Mc;
-    real active_sites = 0.35 * (1 - 2 * 0.3) * siteDensity; 
+    /*real active_sites = 0.35 * (1 - 2 * 0.35) * (2 * NPyr * cellN1); */
 
     real diameter = pow(6 * 32 * mC/(rhoSoot * pi),oneThird);
-    real surface_area = pi * pow(diameter, 2);
-    
-    real delM = 16 * mC;
+    real surface_area = pi * pow(diameter, 2); 
 
-    source = (-2 * k5 * active_sites * cellN1 * surface_area / 16);
+    /*source = (-2 * k5 * active_sites * cellN1 * surface_area / 16); */
+
+    source = -2 * k5 *alpha * chiSoot * surface_area * cellN1/(2* NPyr);
 
     C_UDMI(c,t,51) = source;
 
@@ -118,20 +127,25 @@ DEFINE_SOURCE(first_bin_growth,c,t,dS,eqn)
     real cellM2 = cellRho*C_UDSI(c,t,2);
     real cellN1 = cellRho*C_UDSI(c,t,3);
 
-    int idC2H2 = mixture_specie_index(mat1, "c2h2");
+    Material *mat = THREAD_MATERIAL(t);
+
+    int idC2H2 = mixture_specie_index(mat, "c2h2");
     real C2H2Mf = Pdf_Yi(c,t,idC2H2);
     real C2H2Mc = C2H2Mf*cellRho/C2H2MW;
 
+    real a = a1 + a2*cellTemp;
+    real b = b1 + b2*cellTemp;
+    real alpha = tanh(a/log10(cellM1/cellM0)+b); 
+    if (alpha < 0 ) { alpha = 0.01; }
+
     
     real k4 = A4*pow(cellTemp, n4)*exp(-Ea4/(R*cellTemp)) * C2H2Mc;
-    real active_sites = 0.35 * (1 - 2 * 0.3) * siteDensity; 
 
     real diameter = pow(6 * 32 * mC/(rhoSoot * pi),oneThird);
     real surface_area = pi * pow(diameter, 2);
     
-    real delM = 2 * mC;
 
-    source = (-2 * k4 * active_sites * cellN1 * surface_area / 2);
+    source = -2 * k4 *alpha * chiSoot * surface_area * cellN1/(2* NPyr);
 
     C_UDMI(c,t,50) = source;
 
